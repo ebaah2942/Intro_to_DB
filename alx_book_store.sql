2637f214dd28b2a12b6e906f7d467f19b9a3ef3e
Enoch Baah <ebaah2942@gmail.com>
2024-10-19 10:10:08 +0000

    New touches

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -23,8 +23,10 @@ CREATE TABLE Customers (
 );
 
 CREATE TABLE Orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT FOREIGN KEY REFERENCES Customers
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
