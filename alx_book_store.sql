e94cae203500fbadb7baee387d03cb4fb66adaad
Enoch Baah <ebaah2942@gmail.com>
2024-10-19 09:58:41 +0000

    First commit

---
+++ alx_book_store.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT FOREIGN KEY REFERENCES Authors,
+    price DOUBLE,
+    publication_date DATE
+);
+
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT FOREIGN KEY REFERENCES Customers
+);
+
+CREATE TABLE Order_Details (
+    order_detailid INT PRIMARY KEY,
+    order_id INT FOREIGN KEY REFERENCES Orders,
+    book_id INT FOREIGN KEY REFERENCES Books,
+    quantity INT DOUBLE
+);
